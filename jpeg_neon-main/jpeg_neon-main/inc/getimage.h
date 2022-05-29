4ead1a480d32b7b3ef4af68a8081f2889b99d5ad
Liu Yuxiang <63762379+scarecrow-qf@users.noreply.github.com>
2022-05-30 00:06:59 +0800

    Add files via upload

---
+++ jpeg_neon-main/jpeg_neon-main/inc/getimage.h
@@ -0,0 +1,20 @@
+#ifndef __GETIMAGE_H
+#define __GETIMAGE_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h> //命令在此头文件中
+#include <string.h> //memset需要
+#include <sys/mman.h> //映射相关
+
+int initcamera(const char* device, unsigned char *mptr[4], unsigned int size[4], __u32 format, unsigned int width, unsigned int height);
+int getimage(int fd, struct v4l2_buffer* readbuffer);
+int getfinish(int fd, struct v4l2_buffer* readbuffer);
+
+
+#endif
---
+++ jpeg_neon-main/jpeg_neon-main/src/main.c
@@ -0,0 +1,24 @@
+//#include "jpeg_encoder_neon.h"
+#include "getimage.h"
+#include "jpeg_encoder.h"
+
+int main(void){
+  
+  int fd;
+  const char device[] = "/dev/video0";
+  unsigned char *p[4];
+  unsigned int size[4];
+  unsigned width = 512;
+  unsigned height = 512;
+  unsigned quality = 100;
+  __u32 format = V4L2_PIX_FMT_YUYV;
+  struct v4l2_buffer readbuffer;
+
+  fd = initcamera(device, p, size, format, width, height);
+  getimage(fd, &readbuffer);
+  encode(p[0], width, height, quality);
+  getfinish(fd, &readbuffer);
+  close(fd);
+  return 0;
+}
+
